25c484321d632ca61a7bb7145025ee8c9e36b879
JhonatanTH7 <jhonatantorohurtado93@gmail.com>
2024-05-31 11:10:13 -0500

    Update Assignment

--- src/main/java/com/Platform/Courses/infrastructure/abstract_services/IAssignmentService.java
+++ src/main/java/com/Platform/Courses/infrastructure/abstract_services/IAssignmentService.java
@@ -2,7 +2,9 @@ package com.Platform.Courses.infrastructure.abstract_services;
 
 import com.Platform.Courses.api.dto.request.AssignmentRequest;
 import com.Platform.Courses.api.dto.request.update_request.UpdateAssignmentRequest;
+import com.Platform.Courses.api.dto.response.AssignmentResponse;
 
-public interface IAssignmentService extends CrudService<AssignmentRequest, UpdateAssignmentRequest, Long, Long> {
+public interface IAssignmentService
+        extends CrudService<AssignmentRequest, UpdateAssignmentRequest, AssignmentResponse, Long> {
 
 }
